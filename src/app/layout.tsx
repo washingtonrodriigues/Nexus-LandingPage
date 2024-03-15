7bb4d47a22a750470fb7ab244027cd015027c611
Washington Rodrigues <washington.rodrigues@redgtech.com.br>
2024-03-15 14:12:29 -0300

    feat: add tag google script to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,20 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         ></link>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11431924372"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                gtag('config', 'AW-11431924372');
+              `,
+          }}
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>

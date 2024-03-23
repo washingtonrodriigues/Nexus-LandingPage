78646f0b5959b9a732cb90b6a27cb42022306383
Washington Rodrigues <washington.rodrigues@redgtech.com.br>
2024-03-23 00:32:47 -0300

    chore: add google tag manager to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,15 +32,16 @@ export default function RootLayout({
         ></link>
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11431924372"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16508400084"
         ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() { dataLayer.push(arguments); }
-                gtag('js', new Date());
-                gtag('config', 'AW-11431924372');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'AW-16508400084');
               `,
           }}
         />
